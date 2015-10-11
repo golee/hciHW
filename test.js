99ffe2143bce40467c63847c95ec2a6fb5c1dc08
golee <golee3@gmail.com>
2015-10-11 19:19:07 +0900

    addItem deleteItem

--- test.js
+++ test.js
@@ -1,39 +1,54 @@
-
 var filePath = "C:/Users/Windforce/Dropbox/HciHw2Todolist/todolist.html";
 var filePath = document.title;
 var list = new Array(100);
 
+
+function init() {
+	addItem("Go to school");
+	addItem("Eat Dinner");
+	addItem("Go to the bed");
+	showList();
+}
+
 function showList () {
+	todoList = "";
 	for (i=0; i<100; i++) {
 		if (list[i] != null)
-			document.write(list[i] + "<br>");
-		else {
-			document.write("No more items in the list");
-			break;
-		}
+			todoList += "<tr><td width=400>" +list[i] + "</td><td><button type=\"button\" onClick=\"deleteItem("+i+")\">Delete</button></td></tr>";
+		else 
+			;
 	}
+	if ( todoList === "" )
+		todoList = "<tr><td width=400 style='color:red'>!!Nothing to do</td></tr>";
+	document.getElementById("todoList").innerHTML = todoList;
 }
 
-function getTodoList() {
-}
 
+// item:String
 function addItem (item) {
 	// date, deadlines.
+	if ( item === "" )
+	{
+		alert("Enter some text");
+		return -1;
+	
+	}
 	for (i=0; i<100; i++) {
 		if (list[i] != null)
 			;
 		else {
 			list[i] = item;
-			document.getElementById("systemMessage").innerHTML = "Item added"
+			setSystemMessage("Item added: " + item);
 			break;
 		}
 	}
 }
 
 // Special effect required (crossing-out)
-function deleteItem () {
-	
-	
+// index:Number
+function deleteItem (index) {
+	list[index] = null;
+	showList();
 }
 // Triangle signal
 function halfDeleteItem () {
@@ -51,6 +66,11 @@ function writeMemo () {
 
 function sortItem () {
 	
+}
+
+// message:String
+function setSystemMessage(message) {
+	document.getElementById("systemMessage").innerHTML = message;
 }
 
 /* Optional requirements
